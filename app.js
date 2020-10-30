dd85737f3814c2d9ff0eb663ece2125ab2eedce7
tik-png <apgtik@gmail.com>
2020-10-30 14:54:07 +0000

    first release 1.0.0

---
+++ app.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content.startsWith("!link ")) {
+        let code = msg.content.split(" ")[1];
+        if (!msg.member.voice.channel)
+            msg.reply("Tens de entrar num canal de voz primeiro :no_entry:").then(reply => { reply.delete({ timeout: 10000 }) }).catch(console.error);
+        else if (!code.match(/[A-Z]{6}/g))
+            msg.reply("Código inválido :no_entry:").then(reply => { reply.delete({ timeout: 10000 }) }).catch(console.error);
+        else
+            msg.member.voice.channel.createInvite()
+                .then(invite => {
+                    const gameEmbed = new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setAuthor(msg.member.user.username + " precisa de tripulantes!", msg.member.user.avatarURL())
+                        .setDescription("[**Clica para entrar**](" + invite.url + ")")
+                        //.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+                        .addFields(
+                            { name: ':loud_sound: Canal', value: "Lobby " + msg.member.voice.channel.name.match(/^\d+|\d+\b|\d+(?=\w)/g)[0], inline: true },
+                            { name: ':man_shrugging: Jogadores', value: msg.member.voice.channel.members.size + "/" + msg.member.voice.channel.userLimit, inline: true },
+                            { name: ':map: Codigo', value: code, inline: true },
+                        )
+                        .setFooter('Publica o teu jogo aqui, escreve !link (codigo do jogo)');
+                    msg.reply(gameEmbed).then(msg.delete());
+                }).catch(console.error);
+    }
+});
+
+client.login('TOKEN');
